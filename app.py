5fb8aa42e179865fccf12a5312ff905304bc9788
ksg-dev <ksg.dev.data@gmail.com>
2025-02-03 12:29:19 -0600

    foundation - first run

---
+++ app.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from dash import Dash, dcc, html
+
+
+# Read in data, filter for now since not yet interactive
+data = (
+    pd.read_csv("avocado.csv")
+    .query("type == 'conventional' and region == 'Albany'")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
+    .sort_values(by="Date")
+)
+
+# Initialize Dash app
+app = Dash(__name__)
+
+# Define layout property of app
+app.layout = html.Div(
+    children=[
+        html.H1(children="Avocado Analytics"),
+        html.P(
+            children=(
+                "Analyze the bahavior of avocado prices and the number"
+                " of avocados sold in the US between 2015 and 2018"
+            ),
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
